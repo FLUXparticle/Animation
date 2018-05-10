2f9ee376ca0f14f2540d9c4e613facf32aa876a7
Sven Reinck <sreinck@fluxparticle.de>
2018-05-10 11:28:26 +0200

    datum in der index.html ausgeben

--- servlet/src/main/java/de/fluxparticle/animation/servlet/AnimationPage.java
+++ servlet/src/main/java/de/fluxparticle/animation/servlet/AnimationPage.java
@@ -7,6 +7,9 @@ import de.fluxparticle.animation.servlet.graph.Graph;
 import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+
+import java.time.LocalDate;
 
 /**
  * Created by sreinck on 26.04.18.
@@ -15,6 +18,10 @@ public class AnimationPage extends WebPage {
 
     @Inject
     private Graph graph;
+
+    public AnimationPage() {
+        add(new Label("created", "Created on: " + LocalDate.now()));
+    }
 
     @Override
     public void renderHead(IHeaderResponse response) {
